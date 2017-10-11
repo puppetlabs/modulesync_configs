49447a1d086b9be0e0c58f48f8a75e1c6d4d2fdb
David Swan <david.swan@puppet.co.uk>
2017-10-11 11:17:24 +0100

    spec_helper.rb Rubocop Fix

--- moduleroot/spec/spec_helper.rb
+++ moduleroot/spec/spec_helper.rb
@@ -14,5 +14,5 @@ end
 # put local configuration and setup into spec_helper_local
 begin
   require 'spec_helper_local'
-rescue LoadError
+rescue LoadError # rubocop:disable Lint/HandleExceptions
 end
